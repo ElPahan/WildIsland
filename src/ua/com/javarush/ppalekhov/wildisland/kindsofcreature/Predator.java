a0c1f430208ec72573a1565135761fb73d6469a2
Pavel Palekhov <dlyadel16@yandex.ru>
2022-08-25 21:30:58 +0300

    1)Fix gitignore 2)Add basic classes and interfaces

---
+++ src/ua/com/javarush/ppalekhov/wildisland/kindsofcreature/Predator.java
@@ -0,0 +1,8 @@
+package ua.com.javarush.ppalekhov.wildisland.kindsofcreature;
+
+import ua.com.javarush.ppalekhov.wildisland.action.canEat;
+import ua.com.javarush.ppalekhov.wildisland.action.canMove;
+import ua.com.javarush.ppalekhov.wildisland.action.canReproduction;
+
+public class Predator extends Animal implements canEat, canMove, canReproduction {
+}
